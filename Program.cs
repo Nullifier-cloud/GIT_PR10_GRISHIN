bdd027127faf1bb6dac286bfd74a49fd438eb40a
Grishin <grishin@example.com>
2025-11-13 09:06:40 +0300

    Changed programm.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GIT_PR10_GRISHINMA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Текущая дата: {DateTime.Now:dd.MM.yyyy}");
+        }
+    }
+}
