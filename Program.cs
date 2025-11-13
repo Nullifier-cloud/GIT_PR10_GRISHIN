d54aa921fcd60a40b3fab083d2a11965a72c00e8
Nullifier-cloud <dolblus.v.achello@gmail.com>
2025-11-13 09:58:06 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace GIT_PR10_GRISHINMA
 {
@@ -7,6 +7,8 @@ namespace GIT_PR10_GRISHINMA
         static void Main(string[] args)
         {
             Console.WriteLine($"Текущая дата: {DateTime.Now:dd.MM.yyyy}");
+            Console.WriteLine($"Группа: ИС-23Б");
+            Console.WriteLine($"ФИО: Гришин М.А.");
         }
     }
 }
